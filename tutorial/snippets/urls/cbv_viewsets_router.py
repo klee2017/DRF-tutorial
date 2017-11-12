83e1c92c2ea8aa7fca12f1c90b7aa80df791ddae
Kyeong Lee <kaythechemist@gmail.com>
2017-11-12 18:54:50 +0900

    cbv viewsets and url_explicitly, url_router implement and url_explicitly connected

---
+++ tutorial/snippets/urls/cbv_viewsets_router.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from ..views.cbv_viewsets import SnippetViewSet
+
+router = DefaultRouter()
+router.register(r'', SnippetViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
---
+++ tutorial/snippets/views/cbv_viewsets.py
@@ -0,0 +1,26 @@
+from rest_framework import viewsets, permissions
+
+from ..models import Snippet
+from ..permissions import IsOwnerOrReadOnly
+from ..serializers import SnippetSerializer
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    # @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    # def highlight(self, request, *args, **kwargs):
+    #     snippet = self.get_object()
+    #     return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
