e4ec351a70a25a87b5d506d76eb605f5b20a1e88
Kyeong Lee <kaythechemist@gmail.com>
2017-11-12 15:30:25 +0900

    Snippet 모델에 필드 추가 , save() method 오버라이드 , admin에 Snippet model 등록 and blank true in highlighted

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -15,7 +15,7 @@ class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
-    highlighted = models.TextField()
+    highlighted = models.TextField(blank=True)
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='monokai', max_length=100)
